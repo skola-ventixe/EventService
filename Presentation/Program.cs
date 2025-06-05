294e2c65a203ba28d6fc3571255f84a4c793977b
Tobias Vinther <Tobias.Vinther88@gmail.com>
2025-06-05 09:11:55 +0200

    Enabled Key Vault

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,9 +1,12 @@
-using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Presentation.Data;
 using Presentation.Services;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri")!);
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 builder.Services.AddControllers();
 
@@ -24,7 +27,7 @@ builder.Services.AddHttpClient();
 
 
 builder.Services.AddDbContext<EventDataContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 
 var app = builder.Build();
 
@@ -33,22 +36,6 @@ app.UseHttpsRedirection();
 app.UseCors("AllowAllOrigins");
 
 app.UseAuthorization();
-
-//app.Use(async (context, next) => {
-//    context.Request.EnableBuffering();
-
-//    using (var reader = new StreamReader(
-//        context.Request.Body,
-//        encoding: Encoding.UTF8,
-//        leaveOpen: true))
-//    {
-//        var body = await reader.ReadToEndAsync();
-//        Console.WriteLine($"Request body: {body}");
-//        context.Request.Body.Position = 0;
-//    }
-
-//    await next();
-//});
 
 app.MapControllers();
 
